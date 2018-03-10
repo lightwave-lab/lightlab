fbae3648d6ca56277a7c01ca30f43df793b355fa
Alex Tait <atait@princeton.edu>
2018-03-10 04:32:35 -0500

    Example notebooks. The idea is to show some features, but not unittest. This one is on diode threshold finding. Semi-library practice is demonstrated.

---
+++ notebooks/Examples/ParameterExtraction/myVirtuals.py
@@ -0,0 +1,29 @@
+from lightlab.laboratory.virtualization import VirtualInstrument, DualInstrument
+
+class VirtualKeithley(VirtualInstrument):
+    def __init__(self, viResistiveRef):
+        self.viResistiveRef = viResistiveRef
+        self.__appliedVoltage = 0  # state
+
+    def setVoltage(self, volts):
+        self.__appliedVoltage = volts
+
+    def measCurrent(self):
+        return self.viResistiveRef.ivResponse(self.__appliedVoltage)
+
+
+from lightlab.laboratory.instruments import Keithley  # the hardware instrument class
+class DualKeithley(DualInstrument):
+    real_klass = Keithley
+    virt_klass = VirtualKeithley
+
+    def __init__(self, *args, viResistiveRef=None, **kwargs):
+        super().__init__(
+            real_obj=self.real_klass(*args, **kwargs),
+            virt_obj=self.virt_klass(viResistiveRef))
+
+    def hardware_warmup(self):
+        self.enable(True)
+
+    def hardware_cooldown(self):
+        self.enable(False)
