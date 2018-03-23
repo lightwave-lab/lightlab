8883f22b429e4f33552b8e7ff2717bba47acd348
Thomas Ferreira de Lima <tlima@princeton.edu>
2018-03-22 23:09:50 -0400

    finished testing new logger strategy

--- lightlab/__init__.py
+++ lightlab/__init__.py
@@ -11,19 +11,22 @@ ERROR = logging.ERROR
 CRITICAL = logging.CRITICAL
 
 # inspired from pyvisa
-def log_to_screen(level=DEBUG, logger=logger):
+def log_to_screen(level=INFO, logger=logger):
     logger.setLevel(level)
 
     stream_handlers = [handler for handler in logger.handlers if isinstance(handler, logging.StreamHandler)]
 
     if len(stream_handlers) <= 0:
         ch = logging.StreamHandler()
-        logging.addHandler(ch)
+        logger.addHandler(ch)
     else:
         ch = stream_handlers[0]
-    ch.setLevel(level)
+    ch.setLevel(NOTSET)  # Print all events
     formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(name)s:\n\t%(message)s')
     ch.setFormatter(formatter)
 
+def log_visa_to_screen(level=WARNING):
+    visalogger.setLevel(level)
+
 log_to_screen(INFO, logger)
-visalogger.setLevel(WARNING)
+log_visa_to_screen(WARNING)
