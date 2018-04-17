62fa0b93f2008fa40de83fc5e43b71bf3faf756d
Alex Tait <atait@princeton.edu>
2018-04-17 03:21:40 -0400

    Added notebooks with basic usage of all the instruments that i could reach safely now

---
+++ notebooks/BasicHardwareTests/start.py
@@ -0,0 +1,19 @@
+
+from lightlab.laboratory.state import lab
+# import lightlab.laboratory.state
+# lab = lightlab.laboratory.state.LabState.loadState(filename='/home/jupyter/labstate-newdrivers.json')
+
+from lightlab import log_to_screen, WARNING
+log_to_screen(WARNING)
+
+def start(instrumentName):
+    instr = lab.instruments_dict[instrumentName]
+    if instr.isLive():
+        print('It is alive')
+    else:
+        print('Not responding.')
+    print(instr.driver.instrID())
+    print('Here is what to test:')
+    print('\n'.join(instr.essentialMethods + instr.essentialProperties + instr.implementedOptionals))
+    return instr
+
