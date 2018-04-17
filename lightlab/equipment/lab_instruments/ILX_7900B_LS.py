5d9a174a22c45e8cabf8e5cf80058b40dcb70063
Alex Tait <atait@princeton.edu>
2018-04-16 21:00:18 -0400

    did not like putting the getAsSpectrum method in the Instrument

--- lightlab/equipment/lab_instruments/ILX_7900B_LS.py
+++ lightlab/equipment/lab_instruments/ILX_7900B_LS.py
@@ -5,6 +5,7 @@ import numpy as np
 import time
 
 from lightlab.equipment.abstract_drivers import ConfigModule, MultiModuleConfigurable
+from lightlab.util.data import Spectrum
 from lightlab import visalogger as logger
 
 
@@ -128,6 +129,24 @@ class ILX_7900B_LS(VISAInstrumentDriver, MultiModuleConfigurable):
         minArr = self.getConfigArray('WAVEMIN')
         maxArr = self.getConfigArray('WAVEMAX')
         return tuple(zip(minArr, maxArr))
+
+    def getAsSpectrum(self):
+        ''' Gives a spectrum of power vs. wavelength,
+            which has the wavelengths present as an abscissa,
+            and their powers as ordinate (-120dBm if disabled)
+
+            It starts in dBm, but you can change
+            to linear with the Spectrum.lin method
+
+            Returns:
+                (Spectrum): The WDM spectrum of the present outputs
+        '''
+        absc = self.wls
+        ordi = self.powers
+        for iCh, ena in enumerate(self.enableState):
+            if ena == 0:
+                ordi[iCh] = -120
+        return Spectrum(absc, ordi, inDbm=True)
 
     def allOff(self):
         self.off()
--- lightlab/laboratory/instruments/interfaces.py
+++ lightlab/laboratory/instruments/interfaces.py
@@ -2,7 +2,6 @@
 '''
 
 from .bases import Instrument
-from lightlab.util.data import Spectrum
 
 
 class PowerMeter(Instrument):
@@ -93,6 +92,7 @@ class LaserSource(Instrument):
          'getChannelWls',
          'setChannelPowers',
          'getChannelPowers',
+         'getAsSpectrum',
          'off',
          'allOn']
     essentialProperties = Instrument.essentialProperties + \
@@ -100,25 +100,6 @@ class LaserSource(Instrument):
          'wls',
          'powers']
     optionalAttributes = ['wlRanges', 'allOff']
-
-    def getAsSpectrum(self):
-        ''' Gives a spectrum of power vs. wavelength,
-            which has the wavelengths present as an abscissa,
-            and their powers as ordinate (-120dBm if disabled)
-
-            It starts in dBm, but you can change
-            to linear with the Spectrum.lin method
-
-            Returns:
-                (Spectrum): The WDM spectrum of the present outputs
-        '''
-        absc = self.wls
-        ordi = self.powers
-        for iCh, ena in enumerate(self.enableState):
-            if ena == 0:
-                ordi[iCh] = -120
-        return Spectrum(absc, ordi, inDbm=True)
-
 
 
 class OpticalSpectrumAnalyzer(Instrument):
