ca42ca8eb27572aadab720fbc6e92f214a1e42e6
Alex Tait <atait@princeton.edu>
2018-04-26 12:33:06 -0400

    bumped version in the DOCS. it was outdated

--- docs/conf.py
+++ docs/conf.py
@@ -84,7 +84,7 @@ author = 'Alex Tait, Ethan Gordon, Thomas Ferreira de Lima'
 # built documents.
 #
 # The short X.Y version.
-version = '0.1.0'
+version = '0.1.2'
 # The full version, including alpha/beta/rc tags.
 release = version
 
