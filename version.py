1a796a3ad8a3b41dac037f31703eb17fc304e6bf
Thomas Ferreira de Lima <tlima@princeton.edu>
2018-06-19 19:54:01 -0400

    bumping version

--- version.py
+++ version.py
@@ -1,9 +1,9 @@
 # version.py
 
 # The short X.Y version.
-version = '1.0.3'
+version = '1.0.4'
 # The full version, including alpha/beta/rc tags.
-release = version + ""
+release = version + "dev0"
 
 if __name__ == '__main__':
     print('lightlab v' + version)
