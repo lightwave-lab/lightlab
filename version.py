0f21b209f41bb4be6c91a0e599d4b392a7bbcee8
Thomas Ferreira de Lima <tlima@princeton.edu>
2018-06-19 20:04:26 -0400

    v1.0.3.post0 (reupload to pypi)

--- version.py
+++ version.py
@@ -3,7 +3,7 @@
 # The short X.Y version.
 version = '1.0.3'
 # The full version, including alpha/beta/rc tags.
-release = version + ""
+release = version + ".post0"
 
 if __name__ == '__main__':
     print('lightlab v' + version)
