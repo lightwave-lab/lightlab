2419dbd8fd7aeddbf1e94c40e48668521d1fd859
Thomas Ferreira de Lima <tlima@princeton.edu>
2018-08-13 16:01:47 -0400

    version 1.0.4

--- version.py
+++ version.py
@@ -3,7 +3,7 @@
 # The short X.Y version.
 version = '1.0.4'
 # The full version, including alpha/beta/rc tags.
-release = version + "dev0"
+release = version + ""
 
 if __name__ == '__main__':
     print('lightlab v' + version)
