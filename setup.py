2d695190de570d9a621c7ff6b167e4ce6cdcf054
Thomas Ferreira de Lima <tlima@princeton.edu>
2019-01-02 13:14:42 -0500

    Adding python 3.7 to setup.py tags

--- setup.py
+++ setup.py
@@ -43,6 +43,7 @@ def main():
         author_email="tlima@princeton.edu",
         classifiers=(
             "Programming Language :: Python :: 3.6",
+            "Programming Language :: Python :: 3.7",
             "License :: OSI Approved :: MIT License",
             "Operating System :: OS Independent",
             "Topic :: Scientific/Engineering",
